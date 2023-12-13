f8920c780ff1384e6d95cc1569893b54cf1797c7
Emmanuel Martinez <emmanuelmartinez.freelancer@gmail.com>
2023-12-13 15:50:37 -0600

    Tailwind added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,15 +7,18 @@ import { getAuth } from "firebase/auth";
 // Importa la función para obtener el servicio de Firestore (base de datos) de Firebase
 import { getFirestore } from "firebase/firestore";
 
+//console.log(process.env);
+
 // Configuración de Firebase para tu aplicación web
 // Aquí se definen las claves y los identificadores necesarios para conectar con Firebase
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, // Clave de la API de Firebase
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN, // Dominio de autenticación de Firebase
-  projectId: process.env.FIREBASE_PROJECT_ID, // ID del proyecto en Firebase
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Bucket de almacenamiento en Firebase
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID, // ID del remitente de mensajería en Firebase
-  appId: process.env.FIREBASE_APP_ID // ID de la aplicación en Firebase
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // Clave de la API de Firebase
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, // Dominio de autenticación de Firebase
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID, // ID del proyecto en Firebase
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET, // Bucket de almacenamiento en Firebase
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID, // ID del remitente de mensajería en Firebase
+  appId: process.env.REACT_APP_FIREBASE_APP_ID // ID de la aplicación en Firebase
 };
 
 // Inicializa la aplicación Firebase con la configuración proporcionada
